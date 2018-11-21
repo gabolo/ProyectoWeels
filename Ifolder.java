27c979415e9a0c38c5152c450bce33e1113b3eef
gabolo <gabrielmogu@unisabana.edu.co>
2018-11-21 16:44:48 -0500

    Create Ifolder.java

---
+++ Ifolder.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+package com.example.echo;
+
+/**
+ *
+ * @author ASUS PC
+ */
+public interface Ifolder {
+    public void perform();
+   
+}
